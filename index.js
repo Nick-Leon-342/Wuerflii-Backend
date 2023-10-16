829f5bcddcf6165e2dc6fa0e55f1f7e2c0f6b525
nick <nick.schneider@mmtn-schneider.com>
2023-10-16 23:45:34 +0200

    Added selectsession and sessionpreview

--- index.js
+++ index.js
@@ -38,45 +38,87 @@ const verifyJWT = require('./middleware/verifyJWT')
 app.use(verifyJWT)
 app.post('/game', async (req, res) => {
 
-	const { Attributes, List_Players, List_FinalScores } = req.body
+	const { Attributes, List_Players, FinalScores } = req.body
+	const sessionName = JSON.parse(Attributes).SessionName
 
-	Users.findOne({ where: { id: req.id }, include: Sessions }).then(async (user) => {
-
-		if(user.Sessions.length === 0) {
-
-			//add first session for user
-			Attributes.SessionName = 'session_0'
-			await Sessions.create({ Attributes, List_Players, List_FinalScores: JSON.stringify([List_FinalScores]), userId: req.id }).then(() => {
-				return res.sendStatus(201)
-			}).catch(() => {
-				return res.sendStatus(500)
-			})
-
-		}
-
+	await Users.findOne({ where: { id: req.id }, include: Sessions }).then(async (user) => {
 		for(let i = 0; user.Sessions.length >= i; i++) {
-			if(Attributes.SessionName === '' && !user.Sessions[i]) {
+			const currentSession = user.Sessions[i]
+			if(sessionName === '' && !currentSession) {
 			
 				//add new session with free session_index
-				Attributes.SessionName = `session_${i}`
-				await Sessions.create({ Attributes, List_Players, List_FinalScores: JSON.stringify([List_FinalScores]), userId: req.id }).then(() => {
+				const a = JSON.parse(Attributes)
+				a.SessionName = `session_${i}`
+				await Sessions.create({ 
+					Attributes: JSON.stringify(a), 
+					List_Players, 
+					List_FinalScores: JSON.stringify([FinalScores]), 
+					userId: req.id 
+				}).then(() => {
 					return res.sendStatus(201)
 				}).catch(() => {
 					return res.sendStatus(500)
 				})
+				break
 
-			} else if(user.Sessions[i].Attributes.SessionName === data.Attributes.SessionName) {
+			} else if(currentSession && JSON.parse(currentSession.Attributes).SessionName === sessionName) {
 
 				//update session
+				const tmp = [...JSON.parse(currentSession.List_FinalScores)]
+				tmp.push(FinalScores)
+				const list_updated = JSON.stringify(tmp)
 
+				await currentSession.update({
+					Attributes,
+					List_Players,
+					List_FinalScores: list_updated
+				}).then(() => {
+					return res.sendStatus(200)
+				}).catch(() => {
+					return res.sendStatus(500)
+				})
+				break
+
+			}
+		}
+	}).catch(() => {
+		return res.sendStatus(403)
+	})
+
+})
+
+app.get('/selectsession', async (req, res) => {
+
+	const list_sessions = []
+
+	Users.findOne({ where: { id: req.id }, include: Sessions }).then((user) => {
+		
+		if(!user.Sessions) return res.sendStatus(404)
+		for(const s of user.Sessions) {
+			list_sessions.push({ Attributes: s.Attributes, List_Players: s.List_Players })
+		}
+		return res.json(list_sessions)
+
+	}).catch(() => {
+		return res.sendStatus(403)
+	})
+
+})
+
+app.get('/sessionpreview', async (req, res) => {
+	
+	Users.findOne({ where: { id: req.id }, include: Sessions }).then((user) => {
+
+		const sessionName = req.query.SessionName
+		for(const s of user.Sessions) {
+			if(JSON.parse(s.Attributes).SessionName === sessionName) {
+				return res.json(s.List_FinalScores)
 			}
 		}
 
 	}).catch(() => {
-		res.sendStatus(403)
+		return res.sendStatus(403)
 	})
-
-	return res.sendStatus(500)
 
 })
 
--- models/Sessions.js
+++ models/Sessions.js
@@ -3,15 +3,15 @@
 module.exports = (sequelize, DataTypes) => {
     const Sessions = sequelize.define('Sessions', {
 		Attributes: {
-			type: DataTypes.STRING,
+			type: DataTypes.TEXT,
 			allowNull: false
 		},
         List_Players: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
 		List_FinalScores: {
-			type: DataTypes.STRING,
+			type: DataTypes.TEXT,
 			allowNull: true,
 		},
     })
