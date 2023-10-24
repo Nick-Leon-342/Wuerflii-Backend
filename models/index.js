39df92d07b6f57d3703350d61308555329fb915e
nick <nick.schneider@mmtn-schneider.com>
2023-10-24 23:02:12 +0200

    Added env-variables for sequelize

--- models/index.js
+++ models/index.js
@@ -5,16 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	dialect: process.env.DB_TYPE,
+	logging: process.env.DB_CONSOLE_LOGGING === 'true',
+})
 
 fs
   .readdirSync(__dirname)
