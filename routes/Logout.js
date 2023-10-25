22c12f3595d55a37e6a63c26ff8dc78a1d977257
nick <nick.schneider@mmtn-schneider.com>
2023-10-25 10:23:37 +0200

    Fixed an env-variable error

--- routes/Logout.js
+++ routes/Logout.js
@@ -7,21 +7,20 @@ const { Users } = require('../models')
 
 
 
-
 //__________________________________________________HandleRefreshToken__________________________________________________
 
 router.delete('/', async (req, res) => {
 
     const cookies = req.cookies
-	if (!cookies?.RefreshToken) return res.sendStatus(204)				//check for cookies and check for cookies with 'RefreshToken' properties (name of cookie)	-->since there is no RefreshToken, the logout request is finished
-	const refreshToken = cookies.RefreshToken
+	if (!cookies?.Kniffel_RefreshToken) return res.sendStatus(204)				//check for cookies and check for cookies with 'RefreshToken' properties (name of cookie)	-->since there is no RefreshToken, the logout request is finished
+	const refreshToken = cookies.Kniffel_RefreshToken
 
 	const user = await Users.findOne({ where: { RefreshToken: refreshToken } })
 	const maxAge = (parseInt(process.env.REFRESH_TOKEN_MAX_AGE_IN_MINUTES) || 24 * 60) * 60 * 1000
 
 	//there is no user with that refreshtoken, therefore, the Refreshtoken cookie will be cleared
 	if(!user) {
-		res.clearCookie('RefreshToken', { httpOnly: true, sameSite:'None', maxAge: maxAge, secure: process.env.SSL_OR_LOCALHOST || true })
+		res.clearCookie('Kniffel_RefreshToken', { httpOnly: true, sameSite:'None', maxAge: maxAge, secure: process.env.REFRESH_TOKEN_SECURE === 'true' || true })
 		return res.sendStatus(204)
 	}
 
@@ -29,7 +28,7 @@ router.delete('/', async (req, res) => {
 	const updatedRefreshToken = { RefreshToken: '' }
 	await Users.update(updatedRefreshToken, { where: { RefreshToken: refreshToken } })
 	
-	res.clearCookie('RefreshToken', { httpOnly: true, sameSite:'None', maxAge: maxAge, secure: process.env.SSL_OR_LOCALHOST || true })
+	res.clearCookie('Kniffel_RefreshToken', { httpOnly: true, sameSite:'None', maxAge: maxAge, secure: process.env.REFRESH_TOKEN_SECURE === 'true' || true })
 	res.sendStatus(204)
 
 })
