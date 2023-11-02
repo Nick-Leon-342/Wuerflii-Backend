7cf6d5d27e8efe9c2cb8430a77fc933adabbf0d8
nick <nick.schneider@mmtn-schneider.com>
2023-11-02 21:05:52 +0100

    Added FinalScores as model for sequelize

---
+++ models/FinalScores.js
@@ -0,0 +1,28 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+    const FinalScores = sequelize.define('FinalScores', {
+		Played: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+        Columns: {
+            type: DataTypes.INTEGER,
+			allowNull: false,
+        },
+		Surrender: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+		List_Winner: {
+			type: DataTypes.ARRAY(DataTypes.STRING),
+			allowNull: false,
+		},
+		PlayerScores: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+    })
+
+    return FinalScores
+}
