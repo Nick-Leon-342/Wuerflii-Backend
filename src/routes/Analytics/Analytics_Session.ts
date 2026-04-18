60156c107ab2b063484ac04a598a39fda94ed981
nick <nick.schneider@mmtn-schneider.com>
2026-04-18 12:22:07 +0200

    Fixed Session-Analytics

--- src/routes/Analytics/Analytics_Session.ts
+++ src/routes/Analytics/Analytics_Session.ts
@@ -25,6 +25,54 @@ router.get('', async (req, res) => {
 
 	try {
 		await prisma.$transaction(async (tx) => {
+
+
+			// ____________________ This section gets the time range in which the final_scores should be analyzed ____________________
+
+			const tmp_user = await tx.users.findUnique({ 
+				where: { id: UserID }, 
+				include: {
+					List___Association__Users_And_Sessions: {
+						where: { SessionID: session_id }, 
+						include: { Session: true }
+					}
+				}
+			})
+			if(!tmp_user											) throw new Custom__Handled_Error('User not found.', 404)
+			if(!tmp_user.List___Association__Users_And_Sessions[0]	) throw new Custom__Handled_Error('Session not found.', 404)
+	
+			const tmp_association	= tmp_user.List___Association__Users_And_Sessions[0]
+			const view 				= tmp_association.Statistics__View
+			const view_month		= tmp_association.Statistics__View_Month
+			const view_year 		= tmp_association.Statistics__View_Year
+
+
+			let where = {}
+			if(view === 'STATISTICS_YEAR') {
+				where = {
+					Final_Score: {
+						End: {
+							gte: new Date(`${view_year}-01-01`), 
+							lte: new Date(`${view_year}-12-31T23:59:59.999`), 
+						}
+					}
+				}
+			}
+			if(view === 'STATISTICS_MONTH') {
+				const month = List__Months_Enum.indexOf(view_month)
+				where = {
+					Final_Score: {
+						End: {
+							gte: new Date(Date.UTC(view_year, month, 1)), 
+							lte: new Date(Date.UTC(view_year, month + 1, 0, 23, 59, 59, 999)), 
+						}
+					}
+				}
+			}
+
+
+
+			// ____________________ This section actually gets the final_scores and dedublicates them ____________________
 	
 			const user = await tx.users.findUnique({
 				where: { id: UserID }, 
@@ -34,15 +82,20 @@ router.get('', async (req, res) => {
 						include: {
 							Session: {
 								include: {
+									List___Association__Sessions_And_Players_And_Table_Columns: {
+										select: { PlayerID: true }
+									}, 
 									List___Association__Players_And_FinalScores_And_Sessions: {
 										include: {
 											Player: true, 
 											Final_Score: {
 												include: {
 													List___Association__Players_And_FinalScores_And_Sessions: true
-												}	
+												}, 
 											},
-										}
+										}, 
+										where: where, 
+										distinct: [ 'Final_ScoreID' ] // This prevents dublicates
 									}
 								}
 							}
@@ -51,38 +104,19 @@ router.get('', async (req, res) => {
 				}
 			})
 	
-			if(!user																) throw new Custom__Handled_Error('User not found.', 404)
-			if(!user.List___Association__Users_And_Sessions[0]						) throw new Custom__Handled_Error('Session not found.', 404)
+			if(!user											) throw new Custom__Handled_Error('User not found.', 404)
+			if(!user.List___Association__Users_And_Sessions[0]	) throw new Custom__Handled_Error('Session not found.', 404)
 			const session = user.List___Association__Users_And_Sessions[0].Session
-	
-			if(!session.List___Association__Players_And_FinalScores_And_Sessions[0]	) throw new Custom__Handled_Error('Players not found.', 404)
-	
-			const association__users_and_sessions	= user.List___Association__Users_And_Sessions[0]
-			const statistics__view 					= association__users_and_sessions.Statistics__View
-			const statistics__view_month 			= association__users_and_sessions.Statistics__View_Month
-			const statistics__view_year				= association__users_and_sessions.Statistics__View_Year
-	
-			
-			// __________________________________________________ Search for all finalscores in that selected time __________________________________________________
-	
-			const list__final_scores = []
-			for(const association__users_and_sessions of user.List___Association__Users_And_Sessions) {
-				
-				const session = association__users_and_sessions.Session
-				const list__final_scores__filtered = [...new Map(session.List___Association__Players_And_FinalScores_And_Sessions.map(item => [item.Final_ScoreID, item.Final_Score])).values()] // dedublicate finalscores
 
-				for(const final_score of list__final_scores__filtered) {	
-					const date = new Date(final_score.End)
-					if(statistics__view === 'STATISTICS_YEAR' && date.getFullYear() !== statistics__view_year) break
-					if(statistics__view === 'STATISTICS_MONTH' && (date.getFullYear() !== statistics__view_year || List__Months_Enum[date.getMonth()] !== statistics__view_month)) break
-					list__final_scores.push(final_score)
-				}
-			}
+			const list__final_scores 	= session.List___Association__Players_And_FinalScores_And_Sessions.map(asso => asso.Final_Score)
+			if(list__final_scores.length === 0) throw new Custom__Handled_Error('Data to create statistics is missing.', 409)
 
-			// console.log(statistics__view, statistics__view_month, statistics__view_year, list__final_scores)
+			const list__players			= session.List___Association__Sessions_And_Players_And_Table_Columns.map(association => association.PlayerID)
+			if(!list__players) throw new Custom__Handled_Error('Players not found.', 404)
+
 	
 	
-			// __________________________________________________ Prepare response JSON __________________________________________________
+			// ____________________ Prepare response JSON ____________________
 	
 			type Type__Wins 			= Record<string, number>
 			type Type__Json				= { Games_Played: number, Wins: Type__Wins, Draws: number }
@@ -95,51 +129,64 @@ router.get('', async (req, res) => {
 			const Scores__Highest		: Record<string, number> 		= {}
 			const Scores__Total			: Record<string, number> 		= {}
 			const Data					: Record<string, typeof json>	= {}
+
+
+
+			// ____________________ Init years/months/days of data with zeros ____________________
+
+			if(view === 'STATISTICS_OVERALL') {
+				const start = new Date(session.createdAt).getFullYear()
+				const current = new Date().getFullYear()
+
+				if(current >= start) {
+					for(const year of Array.from({ length: current - start + 1 }, (_, index) => start + index)) {
+						Data[year] = structuredClone(json)
+					}
+				}
+			}
 	
-	
-			// __________________________________________________ Init years/months/days of data with zeros __________________________________________________
-	
-			if(statistics__view === 'STATISTICS_YEAR') {
+			if(view === 'STATISTICS_YEAR') {
 				for(let month = 0; 12 >= month; month++) {
 					Data[month] = structuredClone(json)
 				}
 			}
-			if(statistics__view === 'STATISTICS_MONTH') {
-				const daysInMonth = new Date(statistics__view_year, List__Months_Enum.indexOf(statistics__view_month) + 1, 0).getDate()
+
+			if(view === 'STATISTICS_MONTH') {
+				const daysInMonth = new Date(view_year, List__Months_Enum.indexOf(view_month) + 1, 0).getDate()
 				const list_days = Array.from({ length: daysInMonth }, (_, i) => i + 1)
 				for(const day of list_days) {
 					Data[day] = structuredClone(json)
 				}
 			}
+
+
+
+			// ____________________ Init Scores_... for each player with zero ____________________
 	
-	
-			// __________________________________________________ Init Scores_... for each player with zero __________________________________________________
-	
-			for(const association of session.List___Association__Players_And_FinalScores_And_Sessions) {
-				const player_id = association.PlayerID
-	
+			for(const player_id of list__players) {	
 				Scores__Lowest[player_id] 	= 0
 				Scores__Highest[player_id]	= 0
 				Scores__Total[player_id] 	= 0
 			}
-	
-			
-			// __________________________________________________ Iterate through finalscores and calculate scores, wins etc. __________________________________________________
-	
+
+
+
+			// ____________________ Iterate through finalscores and calculate scores, wins etc. ____________________
+
 			for(const final_score of list__final_scores) {
 				
 				const date 	= new Date(final_score.End)	
 	
 				// Init the selected time -> selected year or month or day
 				let time: number = 0
-				if(statistics__view === 'STATISTICS_OVERALL') time = date.getFullYear()
-				if(statistics__view === 'STATISTICS_YEAR'	) time = date.getMonth() + 1
-				if(statistics__view === 'STATISTICS_MONTH'	) time = date.getDate()
+				if(view === 'STATISTICS_OVERALL') time = date.getFullYear()
+				if(view === 'STATISTICS_YEAR'	) time = date.getMonth() + 1
+				if(view === 'STATISTICS_MONTH'	) time = date.getDate()
 	
 				
 				// Increase games_played count in specific time
 				const target = Data[time]
-				if(!target || target.Games_Played === undefined) throw new Custom__Handled_Error('Something went wrong while creating statistics.', 500)
+				if(!target || target?.Games_Played === undefined) throw new Custom__Handled_Error('Something went wrong while creating statistics.', 500)
 				target.Games_Played++
 	
 	
@@ -175,17 +222,18 @@ router.get('', async (req, res) => {
 				})
 	
 			}
-	
-	
-			// Calculate average scores
-			const list__players = list__final_scores[0]?.List___Association__Players_And_FinalScores_And_Sessions.map(association => association.PlayerID)
+
+
+
+			// ____________________ Calculate average scores ____________________
 			if(list__players) {
 				for(const player_id of list__players) {
 					Scores__Average[player_id] = Math.round((Scores__Total[player_id] || 0) / Total__Games_Played)
 				}
 			}
-	
-	
+
+
+
 			res.json({
 				Total__Games_Played:	Total__Games_Played, 
 				Total__Wins: 			Total__Wins,		// Wins of each player
@@ -201,7 +249,6 @@ router.get('', async (req, res) => {
 
 		})
 	} catch(err) {
-		console.log(err)
 		await handle_error(res, err, 'GET /analytics/session')
 	}
 
